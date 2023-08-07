7a936842c61de940a6063c5a96f8ebd0e89a7953
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2023-08-07 10:58:12 +0200

    /-/ hyphenation issue partly solved

--- grammar/OCR_eval.R
+++ grammar/OCR_eval.R
@@ -4,15 +4,24 @@
 #####################
 library(stringi)
 library(readr)
+library(readtext)
 
+src<-"grammar-preface.pdf_000002"
 #reads OCR text
-tx1<-readLines("grammar-preface.pdf_000002")
+tx1<-readLines(src)
+tx1
+tx1<-readtext(src)
+tx1$text
+#remove -
+tx1<-gsub("-\n","",tx1$text)
+tx1<-gsub("\n"," ",tx1)
+tx1
 #tokenization
-txdf<-stri_split_boundaries(tx1,type="word",simplify = T)
+#txdf<-stri_split_boundaries(tx1,type="word",simplify = T)
 txl<-stri_split_boundaries(tx1,type="word")
 tx2<-unlist(txl)
 tx3<-tx2[tx2!=" "]
-
+tx3
 getwd()
 #writing tokenized text to file
 writeLines(tx3,"grammar02_tok.txt")
@@ -25,12 +34,12 @@ calltt<-"~/pro/treetagger/bin/tree-tagger ~/pro/treetagger/lib/english.par gramm
 system(callrft)
 system(calltt)
 #d3<-read_table("data/lessing-galotti-rftagged_DF.csv",col_names = c("token","tag")) #read sketchenegine export table (vertical) of corpus
-d2<-read_table("grammar02_DFrft.csv",col_names = c("tok","tag")) #read sketchenegine export table (vertical) of corpus
-d3<-read_table("grammar02_DFtt.csv",col_names = c("tok","tag")) #read sketchenegine export table (vertical) of corpus
-d4<-cbind(d2,d3$tok)
+d2<-read_table("grammar02_DFrft.csv",col_names = c("tok","RF_PoStag")) #read sketchenegine export table (vertical) of corpus
+d3<-read_table("grammar02_DFtt.csv",col_names = c("tree_PoStag")) #read sketchenegine export table (vertical) of corpus
+d4<-cbind(d2,d3)
 #d3<-read_table("data/polytimet_DF.csv",col_names = c("tok","tag")) #read sketchenegine export table (vertical) of corpus
 
-set<-d3
+#set<-d3
 
 extraf<-"ſ"
 m<-grep(extraf,d4$tok)
@@ -38,6 +47,9 @@ d4$tok_2<-gsub(extraf,"s",d4$tok)
 m<-grep("f",d4$tok_2)
 d4$f<-NA
 d4$f[m]<-d4$tok_2[m]
-
-
-write.csv(d4,"OCR_eval.csv")
+d5<-edit(d4)
+colnames(d5)[6]<-"f3"
+d5$tok_3<-d5$tok_2
+m<-!is.na(d5$f3)
+d5$tok_3[m]<-d5$f3[m]
+write.csv(d5,"OCR_eval.csv")
