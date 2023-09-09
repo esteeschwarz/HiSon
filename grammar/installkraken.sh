6ad3a36de0c3a118eb0627ba989efc2db6d6889b
esteeschwarz <91368979+esteeschwarz@users.noreply.github.com>
2023-09-09 21:10:33 +0200

    upd

--- grammar/installkraken.sh
+++ grammar/installkraken.sh
@@ -1,6 +1,6 @@
 # 13332.
 # installation workflow for kraken OCR
-pip install kraken
+#pip install kraken
 pip install kraken[pdf] --use-pep517
 #kraken -f pdf -i grammar_p01.pdf grammar.txt binarize segment -bl ocr
 # no model yet
@@ -19,3 +19,7 @@ brew install libvips # dauert
 #kraken -f pdf -i grammar_p01.pdf grammar.txt binarize segment -bl ocr
 # copy models downloaded to library/application support/kraken
 kraken -f pdf -i grammar_p01.pdf grammar.txt binarize segment -bl ocr -m german_print_best.mlmodel
+# mini: 
+pip install pyvips
+# hebrew model BiblIA_01.mlmodel
+kraken get 10.5281/zenodo.5468286
